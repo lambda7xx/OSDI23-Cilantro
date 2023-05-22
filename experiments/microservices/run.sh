328c832524b78c9f57aa994ea031be44e30c14a4
lambda7xx <lambda7xx@gmail.com>
2023-05-22 19:29:50 +0800

    add propfair

--- experiments/microservices/run.sh
+++ experiments/microservices/run.sh
@@ -1,5 +1,5 @@
 et the policy to run - ucbopt, propfair, msile, msevoopt
-POLICY=propfair
+POLICY=ucbopt 
 
 # If running on kind (local cluster)
 ./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
