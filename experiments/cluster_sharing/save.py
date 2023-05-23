f2124af520c0513f22d546c65b89f95b6d3c8d97
lambda7x <lambda7x@node-2.cluster-sharing.ae-osdiatc-23-pg0.clemson.cloudlab.us>
2023-05-23 13:04:03 -0400

    add AE8

---
+++ experiments/cluster_sharing/save.py
@@ -0,0 +1,16 @@
+import time
+import os
+
+while True:
+    # 添加所有更改到暂存区
+    os.system("sudo ./starters/fetch_results.sh")
+    os.system('git add .')
+    
+    # 提交更改，并添加提交信息
+    os.system('git commit -m "add AE8"')
+    
+    # 推送到远程仓库
+    os.system('git push origin AE8')
+    
+    # 暂停两分钟
+    time.sleep(600)
