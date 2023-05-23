8ad622149dd5f068442a165e1ff725eb57f92271
lambda7x <lambda7x@node-2.cluster-sharing.ae-osdiatc-23-pg0.clemson.cloudlab.us>
2023-05-23 13:03:25 -0400

    add AE8

---
+++ experiments/cluster_sharing/run.sh
@@ -0,0 +1,5 @@
+POLICY=utilwelflearn
+# If running on EKS
+# ./starters/launch_cilantro_driver.sh ~/.kube/config $POLICY
+# If running on kind (local cluster)
+./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
