49131ea09e81f55a1b989a58e6b85b8e5f72f727
lambda7x <lambda7x@node-1.ae2.ae-osdiatc-23-pg0.clemson.cloudlab.us>
2023-05-23 12:34:22 -0400

    add  AE3 propfair

--- experiments/microservices/run.sh
+++ experiments/microservices/run.sh
@@ -1,5 +1,5 @@
-et the policy to run - ucbopt, propfair, msile, msevoopt
-POLICY=msevoopt
+#set the policy to run - ucbopt, propfair, msile, msevoopt
+POLICY=propfair
 
 # If running on kind (local cluster)
 ./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
