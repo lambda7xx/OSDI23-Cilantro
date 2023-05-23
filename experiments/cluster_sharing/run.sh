add7688622c6e5151e0e85cf2e3eec74c9e03566
lambda7x <lambda7x@clnode059.clemson.cloudlab.us>
2023-05-23 12:57:52 -0400

    add AE6

---
+++ experiments/cluster_sharing/run.sh
@@ -0,0 +1,6 @@
+# Set the policy to run - propfair, mmf, mmflearn, utilwelforacle, utilwelflearn, evoutil, egalwelforacle, egalwelflearn, evoegal, greedyegal, minerva, ernest, quasar, parties, multincadddec
+POLICY=mmflearn 
+# # If running on EKS
+# ./starters/launch_cilantro_driver.sh ~/.kube/config $POLICY
+# If running on kind (local cluster)
+./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
