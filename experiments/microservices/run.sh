b9c847c5b20f5acb59d35383d397159ed5790546
lambda7xx <lambda7xx@gmail.com>
2023-05-23 11:43:32 +0800

    add msile

--- experiments/microservices/run.sh
+++ experiments/microservices/run.sh
@@ -1,5 +1,5 @@
 et the policy to run - ucbopt, propfair, msile, msevoopt
-POLICY=ucbopt 
+POLICY=msevoopt
 
 # If running on kind (local cluster)
 ./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
