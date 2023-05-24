e6767f9570f985b240f8b089a57dcefee6af3a27
lambda7x <lambda7x@node-3.cluster-sharing.ae-osdiatc-23-pg0.clemson.cloudlab.us>
2023-05-23 23:43:27 -0400

    add AE7

--- experiments/cluster_sharing/run.sh
+++ experiments/cluster_sharing/run.sh
@@ -1,5 +1,5 @@
 # Set the policy to run - propfair, mmf, mmflearn, utilwelforacle, utilwelflearn, evoutil, egalwelforacle, egalwelflearn, evoegal, greedyegal, minerva, ernest, quasar, parties, multincadddec
-POLICY=evoutil
+POLICY=parties
 
 # If running on kind (local cluster)
 ./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
