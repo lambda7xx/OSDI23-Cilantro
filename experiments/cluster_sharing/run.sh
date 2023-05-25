e77b075751055679666e771b2c9d1af170061b18
lambda7x <lambda7x@node-3.sharing.ae-osdiatc-23-pg0.clemson.cloudlab.us>
2023-05-24 22:05:23 -0400

    add AE13

---
+++ experiments/cluster_sharing/run.sh
@@ -0,0 +1,7 @@
+#multincadddec
+# Set the policy to run - propfair, mmf, mmflearn, utilwelforacle, utilwelflearn, evoutil, egalwelforacle, egalwelflearn, evoegal, greedyegal, minerva, ernest, quasar, parties, multincadddec
+POLICY=multincadddec
+# # If running on EKS
+# ./starters/launch_cilantro_driver.sh ~/.kube/config $POLICY
+# If running on kind (local cluster)
+./starters/launch_cilantro_driver_kind.sh ~/.kube/config $POLICY
